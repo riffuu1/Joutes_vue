f6a65c0dbc3fe3c568d71be859320a42364e21e4
riffuu1 <pj85gyl@eduvaud.ch>
2026-03-09 10:38:36 +0100

    correction test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-// test.js (pour JavaScript - avec des erreurs volontaires de style)
-function direBonjour( nom ){
-    console.log( "Bonjour "+nom )
+// test.js corrigé
+function direBonjour(nom) {
+    console.log("Bonjour " + nom);
 }
-direBonjour("ICT324")
+
+direBonjour("ICT324");
