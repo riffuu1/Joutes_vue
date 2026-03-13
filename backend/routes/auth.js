5de1bcb28af965b85592a45a546c9024a6bd02a5
Mouldi Achouri <pv37dbn@eduvaud.ch>
2026-03-13 14:16:25 +0100

    add roles, add limitations for leaders role

---
+++ backend/routes/auth.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../config/db');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET || 'ma_cle_secrete_2plus2egale5';
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const [users] = await pool.execute('SELECT * FROM Users WHERE Username = ?', [username]);
+
+        if (users.length === 0) {
+            return res.status(401).json({ message: "Utilisateur inconnu" });
+        }
+
+        const user = users[0];
+
+        // LOGIQUE CRUE : Comparaison brute pour tes tests
+        if (password !== user.Password) {
+            return res.status(401).json({ message: "Mot de passe incorrect" });
+        }
+
+        // On génère un TOKEN qui contient les infos de l'utilisateur
+        const token = jwt.sign(
+            { id: user.idUsers, role: user.Role },
+            JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+
+        res.json({
+            message: "Connexion réussie",
+            token: token, // Le client devra renvoyer ce token
+            user: {
+                username: user.Username,
+                role: user.Role
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Erreur serveur");
+    }
+});
+
+module.exports = router;
