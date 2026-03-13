d0917f75de68be511e6ee3a83eaad1c72015e583
Mouldi Achouri <pv37dbn@eduvaud.ch>
2026-03-13 14:16:25 +0100

    add roles, add limitations for leaders role

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const authRoutes = require('./routes/auth');
 const playersRoutes = require('./routes/players');
+
+const corsOptions = {
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    optionsSuccessStatus: 200
+}
+
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
+app.use('/api/auth', authRoutes);
 app.use('/api/players', playersRoutes);
 
 const PORT = process.env.PORT || 3006;
