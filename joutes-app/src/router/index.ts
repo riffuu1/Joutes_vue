5c664a2b4904720a749052ca4b0b3cb737ce6b26
Imad El Khattabi <pj85gyl@eduvaud.ch>
2026-03-13 14:28:42 +0100

    feat: Login User View

--- joutes-app/src/router/index.ts
+++ joutes-app/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PlayerTeamView from '../views/PlayerTeamView.vue'
+import LogInUser from "@/views/LogInUser.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LogInUser,
     },
   ],
 })
---
+++ joutes-app/src/views/LogInUser.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="login-container">
+    <div class="login-card">
+
+      <h1>Gestion des Équipes Sportives</h1>
+      <p class="subtitle">Connectez-vous pour continuer</p>
+
+      <div class="form-group">
+        <label>Email</label>
+        <input
+          type="email"
+          placeholder="votre.email@ecnv.ch"
+          v-model="email"
+        />
+      </div>
+
+      <div class="form-group">
+        <label>Mot de passe</label>
+        <input
+          type="password"
+          placeholder="********"
+          v-model="password"
+        />
+      </div>
+
+      <button class="login-btn" @click="login">
+        Se connecter
+      </button>
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+
+const email = ref("")
+const password = ref("")
+
+const login = () => {
+  console.log(email.value, password.value)
+}
+</script>
+
+<style scoped>
+
+.login-container {
+  position: fixed;
+  inset: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-card {
+  width: 420px;
+  padding: 40px;
+  border-radius: 12px;
+  background: white;
+  box-shadow: 0 10px 25px rgba(0,0,0,0.1);
+}
+
+
+h1 {
+  font-size: 28px;
+  margin-bottom: 5px;
+}
+
+.subtitle {
+  color: #6b7280;
+  margin-bottom: 30px;
+}
+
+.form-group {
+  text-align: left;
+  margin-bottom: 20px;
+}
+
+label {
+  font-weight: 500;
+  display: block;
+  margin-bottom: 6px;
+}
+
+input {
+  width: 100%;
+  padding: 12px;
+  border-radius: 8px;
+  border: 1px solid #d1d5db;
+}
+
+input:focus {
+  outline: none;
+  border-color: #2563eb;
+}
+
+.login-btn {
+  width: 100%;
+  background: #2563eb;
+  color: white;
+  border: none;
+  padding: 14px;
+  border-radius: 8px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.login-btn:hover {
+  background: #1e4fd1;
+}
+
+.demo-box {
+  margin-top: 25px;
+  background: #f1f5f9;
+  padding: 15px;
+  border-radius: 8px;
+  font-size: 14px;
+  text-align: left;
+}
+
+</style>
